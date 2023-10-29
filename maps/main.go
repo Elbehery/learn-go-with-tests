aac238b45c5608d47e9459d9fe420374ea46c830
Mustafa Elbehery <melbeher@redhat.com>
2023-10-29 11:54:45 +0100

    refactor

--- maps/main.go
+++ maps/main.go
@@ -1,7 +1,15 @@
 package maps
 
+import "errors"
+
+var ErrKeyNotFound = errors.New("could not find the word you were looking for")
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(key string) string {
-	return d[key]
+func (d Dictionary) Search(key string) (string, error) {
+	v, ok := d[key]
+	if !ok {
+		return "", ErrKeyNotFound
+	}
+	return v, nil
 }
--- maps/main_test.go
+++ maps/main_test.go
@@ -1,14 +1,39 @@
 package maps
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSearch(t *testing.T) {
+	testCases := []struct {
+		name   string
+		key    string
+		exp    string
+		expErr error
+	}{
+		{
+			name:   "key exist",
+			key:    "test",
+			exp:    "this is just a test",
+			expErr: nil,
+		},
+		{
+			name:   "key not exist",
+			key:    "unknown",
+			exp:    "",
+			expErr: ErrKeyNotFound,
+		},
+	}
+
 	d := Dictionary{"test": "this is just a test"}
 
-	got := d.Search("test")
-	want := "this is just a test"
-
-	assertStrings(t, got, want)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			act, err := d.Search(tc.key)
+			assertError(t, err, tc.expErr)
+			assertStrings(t, act, tc.exp)
+		})
+	}
 }
 
 func assertStrings(t testing.TB, got, exp string) {
@@ -16,5 +41,12 @@ func assertStrings(t testing.TB, got, exp string) {
 
 	if got != exp {
 		t.Fatalf("expected %v, but got %v instead", exp, got)
+	}
+}
+
+func assertError(t testing.TB, got, exp error) {
+	t.Helper()
+	if got != exp {
+		t.Fatalf("expected error %v, but got %v instead", exp, got)
 	}
 }
