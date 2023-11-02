7a6361d1ac00b14fd39019c39d5760fb3e1e6c74
Mustafa Elbehery <melbeher@redhat.com>
2023-11-02 18:22:33 +0100

    initian cntxt

---
+++ context/main_test.go
@@ -0,0 +1,32 @@
+package context
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type StubStore struct {
+	response string
+}
+
+func (s *StubStore) Fetch() string {
+	return s.response
+}
+
+func TestServer(t *testing.T) {
+	data := "hello world !!!"
+	svr := Server(&StubStore{data})
+
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	response := httptest.NewRecorder()
+
+	svr.ServeHTTP(response, request)
+
+	exp := data
+	act := response.Body.String()
+
+	if act != exp {
+		t.Errorf("expected %v, but got %v instead", exp, act)
+	}
+}
