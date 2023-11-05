08a72eeb951ab034167ced2f396ab5c6a2d690f6
Mustafa Elbehery <melbeher@redhat.com>
2023-11-05 11:43:18 +0100

    add RecordWin

--- io/myio/file_system_store.go
+++ io/myio/file_system_store.go
@@ -1,15 +1,39 @@
 package myio
 
 import (
+	"encoding/json"
 	"io"
 )
 
 type FileSystemPlayerStore struct {
-	database io.ReadSeeker
+	database io.ReadWriteSeeker
 }
 
 func (f *FileSystemPlayerStore) GetLeague() []Player {
 	f.database.Seek(0, 0)
 	league, _ := NewLeague(f.database)
 	return league
+}
+
+func (f *FileSystemPlayerStore) GetPlayerScore(player string) int {
+	league := f.GetLeague()
+
+	for _, l := range league {
+		if l.Name == player {
+			return l.Wins
+		}
+	}
+	return -1
+}
+
+func (f *FileSystemPlayerStore) RecordWin(player string) {
+	league := f.GetLeague()
+	for i, l := range league {
+		if l.Name == player {
+			league[i].Wins++
+		}
+	}
+
+	f.database.Seek(0, 0)
+	json.NewEncoder(f.database).Encode(&league)
 }
--- io/myio/file_system_store_test.go
+++ io/myio/file_system_store_test.go
@@ -1,29 +1,52 @@
 package myio
 
 import (
+	"io"
+	"os"
 	"reflect"
-	"strings"
 	"testing"
 )
 
 func TestFileSystemStore(t *testing.T) {
-	t.Run("league from a reader", func(t *testing.T) {
-		database := strings.NewReader(`[
+	database := `[
 			{"Name": "Cleo", "Wins": 10},
-			{"Name": "Chris", "Wins": 33}]`)
+			{"Name": "Chris", "Wins": 33}]`
+
+	db, removeFunc := createTempFile(t, database)
+	defer removeFunc()
+	store := FileSystemPlayerStore{database: db}
+
+	t.Run("league from a reader", func(t *testing.T) {
 
 		exp := []Player{
 			{"Cleo", 10},
 			{"Chris", 33},
 		}
 
-		store := FileSystemPlayerStore{database: database}
 		// read first time
 		act := store.GetLeague()
 		assertLeague(t, act, exp)
 		// read second time
 		act = store.GetLeague()
 		assertLeague(t, act, exp)
+	})
+
+	t.Run("get player score", func(t *testing.T) {
+		exp := 33
+		player := "Chris"
+		act := store.GetPlayerScore(player)
+		if act != exp {
+			t.Errorf("expected %v, but got %v instead", exp, act)
+		}
+	})
+
+	t.Run("store wins for existing players", func(t *testing.T) {
+		exp := 34
+		player := "Chris"
+		store.RecordWin(player)
+		act := store.GetPlayerScore(player)
+
+		assertScoreEquals(t, act, exp)
 	})
 }
 
@@ -32,5 +55,30 @@ func assertLeague(t testing.TB, act, exp []Player) {
 
 	if !reflect.DeepEqual(act, exp) {
 		t.Errorf("expected %v, but got %v instead", exp, act)
+	}
+}
+
+func createTempFile(t testing.TB, data string) (io.ReadWriteSeeker, func()) {
+	t.Helper()
+
+	f, err := os.CreateTemp("", "db")
+	if err != nil {
+		t.Fatalf("could not create database temp file, %v", err)
+	}
+
+	f.Write([]byte(data))
+
+	removeFunc := func() {
+		f.Close()
+		os.Remove(f.Name())
+	}
+
+	return f, removeFunc
+}
+
+func assertScoreEquals(t testing.TB, act, exp int) {
+	t.Helper()
+	if act != exp {
+		t.Errorf("got %d want %d", act, exp)
 	}
 }
